2407e31993c2b7504afecd7a93b1fa31373ef9b9
laura <you@example.com>
2024-08-13 11:18:11 -0500

    initial commit

---
+++ printArrayLoop/src/printArrayLoop/printingArrayLoop.java
@@ -0,0 +1,23 @@
+package printArrayLoop;
+import java.util.Arrays;
+public class printingArrayLoop {
+
+	public static void main(String[] args) {
+		//2, 4,7,9 
+		/*String [] candy = {"MNM","gummies","kitkat"};
+		
+		for(int i = 0; i < candy.length; i++ ) {
+			
+			System.out.println(candy[i]);
+		
+		}*/
+
+		
+		int [] num = {2,4,7,9};
+		for(int a=0; a<num.length; a++) {
+			
+			System.out.println(num[a]);
+		
+		}
+	}
+}
